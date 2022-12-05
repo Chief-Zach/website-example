1cb1e94af06b98a68b687201ebb8f7e10cac7b91
Zach Frank <mzmfrank@gmail.com>
2022-12-05 04:20:12 -0500

    Change Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,7 @@
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
+import tensorflow from "../assets/img/tensorflow.jpg"
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
