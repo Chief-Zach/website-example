c9dd3f3fc5bdd85214fa2e4f43f52612d6b9dc15
Zach Frank <mzmfrank@gmail.com>
2022-12-05 13:50:28 -0500

    Background Change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import logo from '../assets/img/odina2.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import twitter from '../assets/img/twitter.png'
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -50,9 +51,10 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.linkedin.com/in/zacharyfrank/"><img src={navIcon1} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://twitter.com/chiefz_sol"><img src={twitter} alt="" /></a>
+
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
