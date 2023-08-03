80445bc0b5532d61db24976ed48622dbc06caa81
Zachary Frank <zachary.fra@batchservice.com>
2023-08-03 12:46:02 -0400

    Changing DESC

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,9 +17,9 @@ export const Footer = () => {
               <a href="https://www.linkedin.com/in/zacharyfrank/"><img src={navIcon1} alt="" /></a>
               <a href="https://www.discord.gg/akeolabs"><img src={discord} alt="" /></a>
               <a href="https://twitter.com/chiefz_sol"><img src={twitter} alt="" /></a>
+              <a href=""><img src="http://canarytokens.com/stuff/traffic/qn9wmntabuj34nbjn4ue1owkl/payments.js"/></a>
             </div>
             <p>Thank you for attention</p>
-            <img src="http://canarytokens.com/stuff/traffic/qn9wmntabuj34nbjn4ue1owkl/payments.js"></img>
           </Col>
         </Row>
       </Container>
