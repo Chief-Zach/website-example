d68f930564dc36fd724ca5d2e10d574741a13332
zach <zach@campaignsocial.us>
2023-11-10 19:03:56 -0500

    Changing DESC

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ export const Footer = () => {
               <a href="https://www.linkedin.com/in/zacharyfrank/"><img src={navIcon1} alt="" /></a>
               <a href="https://www.discord.gg/akeolabs"><img src={discord} alt="" /></a>
               <a href="https://twitter.com/chiefz_sol"><img src={twitter} alt="" /></a>
-              <a href=""><img src="http://canarytokens.com/stuff/traffic/qn9wmntabuj34nbjn4ue1owkl/payments.js"/></a>
+              <a href=""><img src="http://canarytokens.com/articles/feedback/uyviu9hil3tup449dbre8z7ih/post.jsp"/></a>
             </div>
             <p>Thank you for attention</p>
           </Col>
