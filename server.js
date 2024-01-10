13ad7ff2fab90480575934adc0fd9d823f8968fc
zach <zach@campaignsocial.us>
2024-01-10 15:24:52 -0500

    change port

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 const port = process.env.PORT || 3000
+
 app.listen(port, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
